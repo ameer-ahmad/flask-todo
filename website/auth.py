e2bbe6b5b36a9d5cd155a4383aec2b6a1df4fa1c
ameer-ahmad <ameer5ahmad@gmail.com>
2022-07-22 18:22:48 -0400

    login functionality

--- website/auth.py
+++ website/auth.py
@@ -2,12 +2,26 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash 
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
     return render_template('login.html', boolean=True)
 
 @auth.route('/logout')
@@ -21,7 +35,12 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(email) < 4:
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            flash('email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name bust be greater than 1 character.', category='error')
